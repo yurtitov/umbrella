5780771893b90ed03d4ebc6564d227da69f7bb23
Yuriy Titov <yurtitov@gmail.com>
2023-12-29 23:59:55 +0200

    Add Poetry tool

--- src/umbrella.py
+++ src/umbrella.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from argparse import ArgumentParser, Namespace
-from typing import Dict, Any
+from crontab import CronTab
 
 
 def do_info() -> None:
@@ -17,6 +16,10 @@ def do_start() -> None:
     Invoke Start command
     """
     print(f"Command Start")
+    cron = CronTab(user=True)
+    job = cron.new(command='echo Start-start-start')
+    job.minute.every(1)
+    cron.write()
 
 
 def do_stop() -> None:
@@ -36,5 +39,16 @@ def get_command_name() -> str:
     return vars(parser.parse_args()).get(arg_name)
 
 
+def run_command(command_name: str):
+    if command_name == 'info':
+        do_info()
+    elif command_name == 'start':
+        do_start()
+    elif command_name == 'stop':
+        do_stop()
+    else:
+        raise TypeError(f'Unsupported command name: {str}')
+
+
 if __name__ == '__main__':
-    print(get_command_name())
+    run_command(get_command_name())
