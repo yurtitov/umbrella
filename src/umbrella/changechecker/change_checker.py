960530b705b9ec429aedaeb45d999f33264cf095
Yuriy Titov <yurtitov@gmail.com>
2024-01-03 20:38:18 +0200

    WIP. MD5ChangeChecker implementation

--- src/umbrella/changechecker/change_checker.py
+++ src/umbrella/changechecker/change_checker.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 
 class ChangeChecker(ABC):
@@ -11,5 +12,15 @@ class ChangeChecker(ABC):
 class MD5ChangeChecker(ChangeChecker):
     def is_changed(self, folder_path: str) -> bool:
         """ Checks the folder for any content changes by comparison its MD5 checksum"""
+        assert Path(folder_path).is_dir()
+        current_md5 = self.__get_folder_md5(folder_path)
+        previous_md5 = self.__get_previous_folder_md5(folder_path)
+        return current_md5 != previous_md5
+
+    def __get_folder_md5(self, folder_path) -> str:
         # TODO Not implemented yet
-        return True
+        return ""
+
+    def __get_previous_folder_md5(self, folder_path) -> str:
+        # TODO Not implemented yet
+        return ""
